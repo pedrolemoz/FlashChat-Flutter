06b4a1f3917f6f0862870944d2e392a232e78b06
Pedro Lemos <lemozxpedro@gmail.com>
2020-04-28 22:02:57 -0300

    Added a Modal Progress HUD to registration screen

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const id = 'registration_screen';
@@ -14,73 +15,84 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            Center(
-              child: Text(
-                'Create a new account',
-                style: kWelcomeScreenTextStyle.copyWith(fontSize: 30.0),
+              Center(
+                child: Text(
+                  'Create a new account',
+                  style: kWelcomeScreenTextStyle.copyWith(fontSize: 30.0),
+                ),
               ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _emailController,
-              enableSuggestions: true,
-              enableInteractiveSelection: true,
-              textAlign: TextAlign.center,
-              showCursor: false,
-              keyboardType: TextInputType.emailAddress,
-              style: kLabelTextStyle,
-              decoration: kInputDecoration.copyWith(hintText: 'Email'),
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              controller: _passwordController,
-              enableSuggestions: true,
-              enableInteractiveSelection: true,
-              textAlign: TextAlign.center,
-              showCursor: false,
-              keyboardType: TextInputType.visiblePassword,
-              style: kLabelTextStyle,
-              obscureText: true,
-              decoration: kInputDecoration.copyWith(hintText: 'Password'),
-            ),
-            SizedBox(height: 10.0),
-            Hero(
-              tag: 'registerButton',
-              child: RoundedButton(
-                label: 'Register',
-                onPressed: () async {
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: _emailController.text,
-                        password: _passwordController.text);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _emailController,
+                enableSuggestions: true,
+                enableInteractiveSelection: true,
+                textAlign: TextAlign.center,
+                showCursor: false,
+                keyboardType: TextInputType.emailAddress,
+                style: kLabelTextStyle,
+                decoration: kInputDecoration.copyWith(hintText: 'Email'),
+              ),
+              SizedBox(height: 10.0),
+              TextField(
+                controller: _passwordController,
+                enableSuggestions: true,
+                enableInteractiveSelection: true,
+                textAlign: TextAlign.center,
+                showCursor: false,
+                keyboardType: TextInputType.visiblePassword,
+                style: kLabelTextStyle,
+                obscureText: true,
+                decoration: kInputDecoration.copyWith(hintText: 'Password'),
+              ),
+              SizedBox(height: 10.0),
+              Hero(
+                tag: 'registerButton',
+                child: RoundedButton(
+                  label: 'Register',
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (exception) {
+                      print(exception);
                     }
-                  } catch (exception) {
-                    print(exception);
-                  }
-                },
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
